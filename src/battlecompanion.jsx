0af84ba19b183ec38091aedfeac7468831f90480
jtheis85 <jatheis2@gmail.com>
2015-12-27 16:08:01 -0600

    Initial Commit
    Initial commit of BC react app

---
+++ src/battlecompanion.jsx
@@ -0,0 +1,12 @@
+'use strict';
+
+var React    = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    <div>
+        <h1>Battle Companion</h1>
+        <p>Powered by React</p>
+    </div>,
+    document.getElementById('app-root')
+);
