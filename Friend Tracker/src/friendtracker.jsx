ff65486dd31a3ec124aab762b70fe1bbf087ccf0
jtheis85 <jatheis2@gmail.com>
2016-04-25 23:47:19 -0700

    Friend Tracker project seed
    Just the basics to start the project

---
+++ Friend Tracker/src/friendtracker.jsx
@@ -0,0 +1,11 @@
+"use strict";
+
+var React    = require('react');
+var ReactDOM = require('react-dom');
+
+const appRootID = 'app-root';
+
+ReactDOM.render(
+    <h1>Friend Tracker!</h1>,
+    document.getElementById(appRootID)
+);
