6c85c2d0beb1b17e90df61e699abdd9e7bdc3dad
jtheis85 <jatheis2@gmail.com>
2016-04-27 00:51:59 -0700

    React Bootstrap form
    Some basic usage of react bootstrap form stuff

--- Friend Tracker/src/friendtracker.jsx
+++ Friend Tracker/src/friendtracker.jsx
@@ -3,9 +3,30 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
 
+import { Button }       from 'react-bootstrap/lib';
+import { Form }         from 'react-bootstrap/lib';
+import { FormGroup }    from 'react-bootstrap/lib';
+import { ControlLabel } from 'react-bootstrap/lib';
+import { FormControl }  from 'react-bootstrap/lib';
+
 const appRootID = 'app-root';
 
+const AutoComplete = () => (
+    <Form inline>
+        <FormGroup>
+            <ControlLabel>Search by Name</ControlLabel>
+            {' '}
+            <FormControl
+                type="text"
+                placeholder="Higby"
+            />
+        </FormGroup>
+        {' '}
+        <Button bsStyle="primary">Friend Tracker!</Button>
+    </Form>
+);
+
 ReactDOM.render(
-    <h1>Friend Tracker!</h1>,
+    <AutoComplete/>,
     document.getElementById(appRootID)
 );
