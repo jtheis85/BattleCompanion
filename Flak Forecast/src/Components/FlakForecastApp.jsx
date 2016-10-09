4b9e8ec0941c56c5551baf14353cde96141cca50
jtheis85 <jatheis2@gmail.com>
2016-10-09 14:53:40 -0700

    Old Flak Forecast progress

--- Flak Forecast/src/Components/FlakForecastApp.jsx
+++ Flak Forecast/src/Components/FlakForecastApp.jsx
@@ -5,9 +5,9 @@ import React from 'react';
 const FlakForecastApp = React.createClass({
     render() {
         const {route, worlds} = this.props;
-        const worldComponents = worlds && route ? this.props.worlds.map(world => {
-                var className = 'nav-link' + (route.worldId === world.id ? ' _selected' : '');
-                return <a className={className} href={'#' + world.id}>{world.name}</a>
+        const worldComponents = worlds ? this.props.worlds.map(world => {
+                var className = 'nav-link' + (route && route.worldId === world.id ? ' _selected' : '');
+                return <a key={world.id} className={className} href={'#' + world.id}>{world.name}</a>
             }) : null;
         return (
             <div>
